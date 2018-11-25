b532dcac56d1a49b820bd43f76e44092cba41af4
Renáta <re.komendova@gmail.com>
2018-11-25 12:29:08 +0100

    dao

--- src/cz/memorit/dao/MemoritDao.java
+++ src/cz/memorit/dao/MemoritDao.java
@@ -1,5 +1,45 @@
 package cz.memorit.dao;
 
-public class MemoritDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import cz.memorit.bean.Balicek;
+
+public class MemoritDao {
+	private static final String INSERTBALICEK = "INSERT INTO BALICEK(nazev_balicek) VALUES (?)";
+	
+	public void saveBalicek (Balicek novyBalicek) {
+		DataSource ds = getDataSource();
+		
+		try (Connection con = ds.getConnection();
+				PreparedStatement stmt = con.prepareStatement(INSERTBALICEK))
+		{		
+			stmt.setString(1, novyBalicek.getNazev_balicek());
+			stmt.executeUpdate();
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	private DataSource getDataSource( ) {
+		try {
+			Context ctx = new InitialContext();
+			return (DataSource)ctx.lookup("java:/comp/env/jdbc/procvicovacipomuckaResource");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
