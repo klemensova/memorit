5f0b583113f92db9628a6ac6d73930054347f106
martinaklemensova <m.klemensova51@gmail.com>
2018-11-25 12:51:39 +0100

    metoda handle

--- src/cz/memorit/controller/ControllerInterface.java
+++ src/cz/memorit/controller/ControllerInterface.java
@@ -1,5 +1,10 @@
 package cz.memorit.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface ControllerInterface {
+	
+	public void handle(HttpServletRequest request, HttpServletResponse response);
 
 }
