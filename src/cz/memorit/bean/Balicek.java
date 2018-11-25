c5e4de57f3f930452775ebe0304d5e034fccd67d
martinaklemensova <m.klemensova51@gmail.com>
2018-11-25 11:29:02 +0100

    beany pro balicek

--- src/cz/memorit/bean/Balicek.java
+++ src/cz/memorit/bean/Balicek.java
@@ -1,5 +1,22 @@
 package cz.memorit.bean;
 
 public class Balicek {
-	   
+	   public Integer id_balicek;
+	   public String nazev_balicek;
+
+	public String getNazev_balicek() {
+		return nazev_balicek;
+	}
+
+	public void setNazev_balicek(String nazev_balicek) {
+		this.nazev_balicek = nazev_balicek;
+	}
+
+	public Integer getId_balicek() {
+		return id_balicek;
+	}
+
+	public void setId_balicek(Integer id_balicek) {
+		this.id_balicek = id_balicek;
+	}
 }
--- src/cz/memorit/bean/SeznamBalicku.java
+++ src/cz/memorit/bean/SeznamBalicku.java
@@ -1,5 +1,16 @@
 package cz.memorit.bean;
 
+import java.util.ArrayList;
+
 public class SeznamBalicku {
+	public ArrayList<String> baliceklist;
+
+	public ArrayList<String> getBaliceklist() {
+		return baliceklist;
+	}
+
+	public void setBaliceklist(ArrayList<String> baliceklist) {
+		this.baliceklist = baliceklist;
+	}
 
 }
