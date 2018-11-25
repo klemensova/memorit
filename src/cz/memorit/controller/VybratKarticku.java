d322258c6b9a5ab16c4591d92e8fd3114da51603
Renáta <re.komendova@gmail.com>
2018-11-25 17:58:07 +0100

    Načítání kartičky na stránce Kartičky

---
+++ src/cz/memorit/controller/VybratKarticku.java
@@ -0,0 +1,23 @@
+package cz.memorit.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cz.memorit.bean.Karticka;
+import cz.memorit.bean.SeznamKarticek;
+import cz.memorit.dao.MemoritDao;
+
+public class VybratKarticku implements ControllerInterface{
+	private MemoritDao instanceDao = new MemoritDao();
+	
+	public String handle(HttpServletRequest request, HttpServletResponse response) {
+		Integer idBalicku = (Integer)request.getSession().getAttribute("id_balicek");
+		SeznamKarticek seznam = instanceDao.loadKarticka(idBalicku, 1);
+		if(seznam != null) {
+			Karticka nactenaKarticka = seznam.getKartaList().get(0);
+			request.setAttribute("randomKarticka", nactenaKarticka);
+		} 
+		return "/karticky.jsp";
+	}
+	
+}
